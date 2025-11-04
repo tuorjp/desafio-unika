af77a80b8e8ca59b51dacfd09a29698127fc1669
João Pedro <jplmarotinho22@gmail.com>
2025-11-04 09:48:47 -0300

    Chamada teste no AppComponent

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {CommonModule} from "@angular/common";
+import {ClienteService} from "./services/cliente.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(private clienteService: ClienteService) {
+  }
+
+  carregarClientes(): void {
+    console.log('TESTE');
+    this.clienteService.listarTodos()
+      .subscribe({
+        next: (clientes) => {
+          console.log('Clientes: ', clientes);
+        },
+        error: (erro) => {
+          console.error(erro)
+        },
+        complete: () => {
+          console.log('Requisição concluída')
+        }
+      })
+  }
+
+  ngOnInit(): void {
+    this.carregarClientes()
+  }
 }
